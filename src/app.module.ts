74675292d12f2293b8109a70b933796653c00b94
supriwahyu <supriwahyu95@gmail.com>
2026-07-21 11:11:03 +0700

    create prisma service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ArticlesModule],
+  imports: [ArticlesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
