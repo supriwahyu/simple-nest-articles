ec095f6328eacf4e2c2b141bf3c2c62906ada14e
supriwahyu <supriwahyu95@gmail.com>
2026-07-23 09:11:52 +0700

    config the mysql.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -12,6 +12,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
         user: process.env.DB_USER!,
         password: process.env.DB_PASSWORD!,
         database: process.env.DB_NAME!,
+        allowPublicKeyRetrieval: true
       }),
     });
   }
