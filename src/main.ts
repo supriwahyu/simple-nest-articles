445c4ce7612aca5e9224276d6f0ec63ea464eae5
supriwahyu <supriwahyu95@gmail.com>
2026-08-19 08:38:48 +0700

    new package swagger has been installed.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Articles API')
+    .setDescription('REST API for managing articles')
+    .setVersion('1.0')
+    .addTag('Articles')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 
   app.useGlobalPipes(
